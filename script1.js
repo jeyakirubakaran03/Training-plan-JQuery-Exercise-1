c77ed64b6e581bf7083e995f526440bbdb3e28c5
jeyakirubakaran03 <jeyakirubakaran03@gmail.com>
2025-08-22 18:12:32 +0530

    modified

--- script1.js
+++ script1.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
-    $(".jquery-reference-hidden").removeClass("jquery-reference-hidden");
+    // $("#jquery-reference-hidden").removeClass("jquery-reference-hidden");
+    
+    $("#reference, #reference-section").removeClass("jquery-reference-hidden");
     
     /* **********Changing default colors*********** */
     
-    $(".mypage-header-nav, .leftnav, .center-content, .js-wrapper, .mypage-footer, button, .responsive-heading").addClass("bg-color table-border-color text-color");
-    $(".section-content, .section-heading, .project-title, .project-tech, .project-tech-name, .js-container, .mypage-footer-a, .js-heading").addClass("text-color");
+    // $(".mypage-header-nav, .leftnav, .center-content, .js-wrapper, .mypage-footer, button, .responsive-heading,.section-content, .section-heading, .project-title, .project-tech, .project-tech-name, .js-container, .mypage-footer-a, .js-heading ").addClass("bg-color table-border-color text-color");
+    // $(".section-content, .section-heading, .project-title, .project-tech, .project-tech-name, .js-container, .mypage-footer-a, .js-heading").addClass("text-color");
     
+    $(".theme-element").addClass("bg-color table-border-color text-color");
     $("input").removeClass("js-input-field-border");
 
     $(".submit-button").hover(function(){
