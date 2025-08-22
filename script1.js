0cf14c182668a722a31db9e2064a6fa863e9e562
jeyakirubakaran03 <jeyakirubakaran03@gmail.com>
2025-08-22 09:53:01 +0530

    Initial commit

---
+++ script1.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function(){
+ 
+
+    /* **********Adding new div*********** */
+
+    $(`<div>`,{
+        html: '<aside class="leftnav"><h3>References</h3> </aside>'
+    }).appendTo("#mypage-leftnav");
+
+    $(`<div>`, {
+        html: `<section class="center-content">
+                <h3 class="responsive-heading">References</h3>
+                <ul class="list">
+                    <li><span class="course">Name:</span><span class="year"> Shanmugavadivu</span></li>
+                    <li><span class="course">Designation:</span><span class="year">Manager</span></li>
+                    <li><span class="course">Email ID: </span><span class="year">svadivu@gmail.com</span></li>
+                    <li><span class="course">Phone: </span><span class="year">9865432123</span></li>
+                </ul>
+                </section>`
+    }).appendTo("main");
+
+    /* **********Changing default colors*********** */
+    $(".mypage-header-nav, .leftnav, .center-content, .js-wrapper, .mypage-footer, button, .responsive-heading").addClass("bg-color table-border-color");
+    $(".mypage-header-nav, .leftnav, .center-content, .year, .course, .title, .tech, .name, .mypage-footer, .responsive-heading, .container, button, a, .js-heading").addClass("text-color");
+    $("input").addClass("input-border");
+
+    $(".submit-button").hover(function(){
+        $(this).toggleClass("button-color");
+    })
+
+    /* ******************Hiding Footer Section************************** */
+
+    $("#mypage-footer").hide();
+})
+
